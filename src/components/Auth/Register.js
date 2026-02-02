fe6ba9fd7cd2636dcf2c5f2a312ee8e2954ff1fe
maxjeffwell <maxjeffwell@gmail.com>
2026-02-02 05:57:31 -0500

    Add red/coral background to login and register pages

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -9,8 +9,10 @@ const AuthContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  min-height: 80vh;
+  min-height: 100vh;
   padding: 2rem;
+  background-color: #fa625f;
+  box-sizing: border-box;
 `;
 
 const AuthForm = styled.form`
